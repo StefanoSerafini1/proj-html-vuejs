2546b249a3f45e189181e6451fe020274d9ef49f
StefanoSerafini <stefanoserafini7@gmail.com>
2020-12-02 23:59:28 +0100

    aggiunta vue li footer and nav bar

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        // Elenco recent post
+        posts: [
+            'The best protein shake',
+            'Ultimate cardio workout',
+            'New juices available now',
+            'Tips to find training partners',
+            '20 best healthy recipies'
+        ],
+        links: [
+            'Home',
+            'Service',
+            'About',
+            'Videos',
+            'Blog'
+        ]
+      }
+});
